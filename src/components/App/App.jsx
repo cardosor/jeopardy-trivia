e234535bb57bcf0cdcadc6efd648a3ab225e81ab
Rafael Cardoso <usacb@hotmail.com>
2022-06-11 22:29:40 -0500

    Added Material UI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,9 @@ import './App.css';
 import { useState } from 'react';
 import Question from '../Question/Question';
 import React from 'react';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 function App() {
 
@@ -24,9 +27,9 @@ function App() {
 
   const handleScore = (e) => {
     console.log(e.target.name);
-    console.log(e.target.name === "inc" ? questionObj.points :  questionObj.points*-1)
+    console.log(e.target.name === "inc" ? questionObj.points : questionObj.points * -1)
     let temp = questionObj.points;
-    const tempScore = score + (e.target.name === "inc" ? temp :  temp*-1);
+    const tempScore = score + (e.target.name === "inc" ? temp : temp * -1);
     setScore(tempScore);
   }
 
@@ -36,16 +39,16 @@ function App() {
   }
 
   const handleClickNewQuestion = async () => {
-      const URL = "http://jservice.io/api/random";
-      setShowQuestion(false);
-      try {
-        const result = await fetch(URL);
-        const data = await result.json();
-        console.log(data);
-        if (!data[0].value || !data[0].answer || !data[0].question || !data[0].category.title) {
-          console.log("tried again");
-          handleClickNewQuestion();
-        } else {
+    const URL = "http://jservice.io/api/random";
+    setShowQuestion(false);
+    try {
+      const result = await fetch(URL);
+      const data = await result.json();
+      console.log(data);
+      if (!data[0].value || !data[0].answer || !data[0].question || !data[0].category.title) {
+        console.log("tried again");
+        handleClickNewQuestion();
+      } else {
         const obj = {
           question: data[0].question,
           answer: data[0].answer,
@@ -55,26 +58,65 @@ function App() {
         }
         setQuestionObj(obj);
       }
-      } catch (e) {
-        console.log(e);
-      }
+    } catch (e) {
+      console.log(e);
+    }
   }
   return (
     <div className="App">
-      <h1>Score: {score}</h1>
-      {
-        showQuestion ?
-          <React.Fragment>
-            <button onClick={handleScore} name="inc">I got it</button>
-            <button onClick={handleScore} name="dec">I did not get it</button>
-          </React.Fragment>
-        :
-          <React.Fragment>
-          </React.Fragment>
-      }
-      <button onClick={handleClickNewQuestion}>Random Trivia Question</button>
-      <button onClick={handleClickResetScore}>Reset Score</button>
-      <Question questionObj={questionObj} handleToggle={handleToggle} />
+      <div>
+        <Box  minWidth={(theme) => theme.breakpoints.values.lg}
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignContent: 'center',
+            justifyContent: 'center',
+            p: 1,
+            m: 1,
+            maxWidth: 1,
+            height: 800,
+            borderRadius: 1,
+          }}
+        >
+          <Paper elevation={24}
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              maxWidth: .8,
+              minWidth:.8,
+              height: 600,
+              p:1,
+              alignContent: 'center',
+              justifyContent: 'center',
+              bgcolor: '#8E44BB',
+              p: 2
+            }}>
+            <div>
+              <h1>Score: {score}</h1>
+              {
+                showQuestion ?
+                  <div className='right-wrong-container'>
+                    <Button variant="contained" onClick={handleScore} color="success" name="inc">Got It Right</Button>
+                    <Button variant="contained" onClick={handleScore} color="error" name="dec">Got It Wrong</Button>
+                  </div>
+                  :
+                  <React.Fragment>
+                  </React.Fragment>
+              }
+              <p />
+              <div className='center-btn-container'>
+                <Button variant="contained" onClick={handleClickNewQuestion}>Get A Random Trivia Question</Button>
+              </div>
+              <Question questionObj={questionObj} handleToggle={handleToggle} />
+              <div className='reset-score-btn'>
+                {
+                  score !== 0 && <Button variant="contained" onClick={handleClickResetScore}>Reset Score</Button>
+                }
+              </div>
+            </div>
+          </Paper>
+        </Box>
+      </div>
     </div>
   );
 }
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import Button from '@mui/material/Button';
+import React from 'react';
 
 function Question({ questionObj, handleToggle}) {
     console.log(questionObj.question === "" ? "click to play" : "playing");
     return (
-            questionObj.question === "" ?
-                <h1>Click button to play!</h1>
-            :
-            <div className="question">
-                <h1>Answer: {questionObj.question}</h1>
-                <h1>Category: {questionObj.category}</h1>
-                <h1>Points: {questionObj.points}</h1>
-                {questionObj.showQuestion && <h1>Question: What is {questionObj.answer}</h1>}
-                <button onClick={handleToggle}>Show Question!</button>
-             </div>
+        <React.Fragment>
+            {
+                questionObj.question === "" ?
+                    <h1>Click button to play!</h1>
+                :
+                    <React.Fragment>
+                    <h1>Answer: {questionObj.question}</h1>
+                    <h1>Category: {questionObj.category}</h1>
+                    <h1>Points: {questionObj.points}</h1>
+                    <div className='center-btn-container'>
+                    <Button variant="contained" onClick={handleToggle}>Show Question!</Button>
+                    </div>
+                    {questionObj.showQuestion && <h1>Question:{questionObj.answer}</h1>}
+                </React.Fragment>
+            }
+        </React.Fragment>
     );
 }
 
